78c15efd8aefe5cdaef8c226d13588afe50ed62e
pbrsa <pbrsa@LNVYOG7>
2022-09-26 20:52:14 +0200

    first commit, add files

---
+++ include/motor.h
@@ -0,0 +1,20 @@
+/*
+ * motor.h
+ *
+ *  Created on: 29. 9. 2020
+ *      Author: dosedel
+ */
+
+#ifndef MOTOR_H_
+#define MOTOR_H_
+
+#include <msp430.h>
+// macros definition
+
+
+// functions prototypes
+
+
+// variables
+
+#endif /* MOTOR_H_ */
